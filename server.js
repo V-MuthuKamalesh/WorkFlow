3430a99387f531c8000e2ec324a6ccc168400839
MuthuKamalesh32 <muthukamalesh@finestcoder.com>
2025-01-06 13:12:14 +0530

    updated server

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const cors= require('cors');
 require('dotenv').config();
 const userRoutes = require('./src/routes/user');
-const workroutes = require('./src/routes/work');
+const { server } = require('./src/routes/work'); 
+// const workroutes = require('./src/routes/work');
 const app = express();
 
 app.use(express.json());
@@ -14,9 +15,9 @@ mongoose.connect(process.env.DATABASE_URL)
   .catch(err => console.log('Error connecting to MongoDB', err));
 
 app.use('/api/users', userRoutes); 
-app.use('/api/work',workroutes);
+// app.use('/api/work',workroutes);
 
 const PORT = process.env.PORT;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
