aaa879a1371e75b2325d99a5ec1245765e048378
MuthuKamalesh32 <muthukamalesh@finestcoder.com>
2025-01-27 21:19:00 +0530

    updated websocket

--- src/routes/work.js
+++ src/routes/work.js
@@ -6,206 +6,210 @@ const router = express.Router();
 module.exports = (io) => {
 
     io.use(socketAuthMiddleware);
-    
+
     io.on('connection', (socket) => {
         console.log('A user connected');
         const adminId = socket.userId;
-        // Work-related events
+        
+        const emitResponse = (eventName, data, callback) => {
+            io.emit(eventName, data); 
+            callback(data); 
+        };
+
+        // Workspace events
         socket.on('getWorkspaces', async (data, callback) => {
-            const {moduleId} = data;
+            const { moduleId } = data;
             const workspaces = await workspaceController.getWorkspaces(adminId, moduleId);
-            callback(workspaces);
+            emitResponse('getWorkspaces', workspaces, callback);
         });
 
         socket.on('getDashboardDetails', async (data, callback) => {
-            const {moduleId, userId, workspaceId} = data;
+            const { moduleId, userId, workspaceId } = data;
             const workspaces = await workspaceController.getWorkspacesWithItemCounts(userId, moduleId, workspaceId);
-            callback(workspaces);
+            emitResponse('getDashboardDetails', workspaces, callback);
         });
 
         socket.on('getBoardsByWorkspaceById', async (data, callback) => {
-            const {id}=data;
+            const { id } = data;
             const workspace = await workspaceController.getWorkspaceById(id);
-            callback(workspace);
+            emitResponse('getBoardsByWorkspaceById', workspace, callback);
         });
-        
+
         socket.on('getWorkspaceDetailsById', async (data, callback) => {
-            const {id}=data;
+            const { id } = data;
             const workspace = await workspaceController.getWorkspaceDetailsById(id);
-            callback(workspace);
+            emitResponse('getWorkspaceDetailsById', workspace, callback);
         });
 
         socket.on('createWorkspace', async (data, callback) => {
-            const {moduleId , workspaceData} =data;
+            const { moduleId, workspaceData } = data;
             workspaceData.moduleId = moduleId;
             const workspace = await workspaceController.createWorkspace(workspaceData);
-            callback(workspace);
+            emitResponse('createWorkspace', workspace, callback);
         });
 
         socket.on('updateWorkspaceById', async (data, callback) => {
             const { id, updateData } = data;
             const updatedWorkspace = await workspaceController.updateWorkspace(id, updateData);
-            callback(updatedWorkspace);
+            emitResponse('updateWorkspaceById', updatedWorkspace, callback);
         });
 
         socket.on('deleteWorkspaceById', async (data, callback) => {
-            const  {id, moduleId}  = data;
+            const { id, moduleId } = data;
             const result = await workspaceController.deleteWorkspace(id, moduleId);
-            callback(result);
+            emitResponse('deleteWorkspaceById', result, callback);
         });
 
         // Board events
         socket.on('addBoardToWorkspace', async (data, callback) => {
             const { id, board } = data;
             const updatedWorkspace = await workspaceController.addBoardToWorkspace(id, board);
-            callback(updatedWorkspace);
+            emitResponse('addBoardToWorkspace', updatedWorkspace, callback);
         });
 
         socket.on('removeBoardFromWorkspace', async (data, callback) => {
             const { boardId } = data;
             const updatedWorkspace = await workspaceController.removeBoardFromWorkspace(boardId);
-            callback(updatedWorkspace);
+            emitResponse('removeBoardFromWorkspace', updatedWorkspace, callback);
         });
 
         socket.on('updateBoardInWorkspace', async (data, callback) => {
             const { boardId, updateData } = data;
             const updatedBoard = await workspaceController.updateBoardInWorkspace(boardId, updateData);
-            callback(updatedBoard);
+            emitResponse('updateBoardInWorkspace', updatedBoard, callback);
         });
 
         socket.on('getBoardById', async (data, callback) => {
-            const { boardId} = data;
+            const { boardId } = data;
             const board = await workspaceController.getBoardById(boardId);
-            // console.log("Board", board);
-            
-            callback(board);
+            emitResponse('getBoardById', board, callback);
         });
 
         // Group events
         socket.on('addGroupToBoard', async (data, callback) => {
             const { boardId, group, itemId } = data;
             const updatedBoard = await workspaceController.addGroupToBoard(boardId, group, itemId);
-            callback(updatedBoard);
+            emitResponse('addGroupToBoard', updatedBoard, callback);
         });
 
         socket.on('removeGroupFromBoard', async (data, callback) => {
             const { groupId, type } = data;
             const updatedBoard = await workspaceController.removeGroupFromBoard(groupId, type);
-            callback(updatedBoard);
+            emitResponse('removeGroupFromBoard', updatedBoard, callback);
         });
 
         socket.on('updateGroupInBoard', async (data, callback) => {
             const { groupId, updateData } = data;
             const updatedGroup = await workspaceController.updateGroupInBoard(groupId, updateData);
-            callback(updatedGroup);
+            emitResponse('updateGroupInBoard', updatedGroup, callback);
         });
 
         // Item events
         socket.on('addItemToGroup', async (data, callback) => {
             const { groupId, item, type, boardId } = data;
             const updatedGroup = await workspaceController.addItemToGroup(groupId, item, type, boardId);
-            callback(updatedGroup);
+            emitResponse('addItemToGroup', updatedGroup, callback);
         });
 
         socket.on('createItem', async (data, callback) => {
             const { item, type, boardId } = data;
             const itemData = await workspaceController.addItem(item, type, boardId);
-            callback(itemData);
+            emitResponse('createItem', itemData, callback);
         });
 
         socket.on('removeItemFromGroup', async (data, callback) => {
             const { itemId, type } = data;
             const updatedGroup = await workspaceController.removeItemFromGroup(itemId, type);
-            callback(updatedGroup);
+            emitResponse('removeItemFromGroup', updatedGroup, callback);
         });
 
         socket.on('updateItemInGroup', async (data, callback) => {
             const { itemId, updateData, type, boardId } = data;
             const updatedItem = await workspaceController.updateItemInGroup(itemId, updateData, type, boardId, adminId);
-            callback(updatedItem);
+            emitResponse('updateItemInGroup', updatedItem, callback);
         });
 
         socket.on('addMembersToItem', async (data, callback) => {
             const { itemId, userId, type } = data;
             const updatedItem = await workspaceController.addMembersToItem(itemId, userId, type, adminId);
-            callback(updatedItem);
+            emitResponse('addMembersToItem', updatedItem, callback);
         });
 
         socket.on('removeMembersFromItem', async (data, callback) => {
             const { itemId, userId, type } = data;
             try {
                 const updatedItem = await workspaceController.removeMembersFromItem(itemId, userId, type, adminId);
-                callback(updatedItem);
+                emitResponse('removeMembersFromItem', updatedItem, callback);
             } catch (err) {
                 callback({ error: err.message });
             }
-        });        
+        });
 
         socket.on('addFavouriteWorkspace', async (data, callback) => {
             const { workspaceId, type } = data;
             try {
                 const updatedFavourite = await workspaceController.addFavouriteWorkspace(workspaceId, type);
-                callback(updatedFavourite);
+                emitResponse('addFavouriteWorkspace', updatedFavourite, callback);
             } catch (err) {
                 callback({ error: err.message });
             }
-        });  
+        });
 
         socket.on('removeFavouriteWorkspace', async (data, callback) => {
             const { workspaceId, type } = data;
             try {
                 const updatedFavourite = await workspaceController.removeFavouriteWorkspace(workspaceId, type);
-                callback(updatedFavourite);
+                emitResponse('removeFavouriteWorkspace', updatedFavourite, callback);
             } catch (err) {
                 callback({ error: err.message });
             }
-        });  
+        });
 
         socket.on('addBoardToFavourite', async (data, callback) => {
             const { boardId, type } = data;
             try {
                 const updatedFavourite = await workspaceController.addBoardToFavourite(boardId, type);
-                callback(updatedFavourite);
+                emitResponse('addBoardToFavourite', updatedFavourite, callback);
             } catch (err) {
                 callback({ error: err.message });
             }
-        });  
+        });
 
         socket.on('removeBoardFromFavourite', async (data, callback) => {
             const { boardId, type } = data;
             try {
                 const updatedFavourite = await workspaceController.removeBoardFromFavourite(boardId, type);
-                callback(updatedFavourite);
+                emitResponse('removeBoardFromFavourite', updatedFavourite, callback);
             } catch (err) {
                 callback({ error: err.message });
             }
-        });  
+        });
 
         socket.on('getFavourite', async (data, callback) => {
-            const { userId,type } = data;
+            const { userId, type } = data;
             try {
                 const favourite = await workspaceController.getFavourite(userId, type);
-                callback(favourite);
+                emitResponse('getFavourite', favourite, callback);
             } catch (err) {
                 callback({ error: err.message });
             }
-        });  
+        });
 
         socket.on('isBoardInFavourite', async (data, callback) => {
             const { boardId, type } = data;
             try {
                 const updatedFavourite = await workspaceController.isBoardInFavourite(boardId, type);
-                callback(updatedFavourite);
+                emitResponse('isBoardInFavourite', updatedFavourite, callback);
             } catch (err) {
                 callback({ error: err.message });
             }
         });
-        
+
         socket.on('isWorkspaceInFavourite', async (data, callback) => {
             const { workspaceId, type } = data;
             try {
                 const updatedFavourite = await workspaceController.isWorkspaceInFavourite(workspaceId, type);
-                callback(updatedFavourite);
+                emitResponse('isWorkspaceInFavourite', updatedFavourite, callback);
             } catch (err) {
                 callback({ error: err.message });
             }
@@ -214,25 +218,26 @@ module.exports = (io) => {
         socket.on('getNotifications', async (data, callback) => {
             try {
                 const notifications = await workspaceController.getNotifications(adminId);
-                callback(notifications);
+                emitResponse('getNotifications', notifications, callback);
             } catch (err) {
                 callback({ error: err.message });
             }
         });
-        
+
         socket.on('updateNotifications', async (data, callback) => {
             const { notifications } = data;
             try {
                 const updatedUser = await workspaceController.updateNotifications(adminId, notifications);
-                callback(updatedUser);
+                emitResponse('updateNotifications', updatedUser, callback);
             } catch (err) {
                 callback({ error: err.message });
             }
         });
-        
+
         socket.on('disconnect', () => {
             console.log('A user disconnected');
         });
     });
+
     return router;
 };
